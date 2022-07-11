52dc352c8b303d54e680b21d28f757f2f1eeeca3
adv4new <107744568+adv4new@users.noreply.github.com>
2022-07-11 22:47:27 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -23,10 +23,10 @@ async def start(client, message):
     if message.chat.type in ['group', 'supergroup']:
         buttons = [
             [
-                InlineKeyboardButton('ᴄʜᴀɴɴᴇʟ', url='https://t.me/Mh_linkZzz')
+                InlineKeyboardButton('🍂 ᴄʜᴀɴɴᴇʟ', url='https://t.me/Mh_linkZzz')
             ],
             [
-                InlineKeyboardButton('ʜᴇʟᴘ', url=f"https://t.me/{temp.U_NAME}?start=help"),
+                InlineKeyboardButton('🍀 ʜᴇʟᴘ', url=f"https://t.me/{temp.U_NAME}?start=help"),
             ]
             ]
         reply_markup = InlineKeyboardMarkup(buttons)
@@ -42,13 +42,13 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
         buttons = [[
-            InlineKeyboardButton('ᴍʏ ғᴀᴛʜᴇʀ', url=f'https://t.me/thilak2003')
+            InlineKeyboardButton('🍁 ᴍʏ ғᴀᴛʜᴇʀ', url=f'https://t.me/thilak2003')
             ],[
             InlineKeyboardButton('🔍 sᴇᴀʀᴄʜ', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('ɢʀᴏᴜᴘ', url='https://t.me/moviereqgroup_movieshub')
+            InlineKeyboardButton('🌿 ɢʀᴏᴜᴘ', url='https://t.me/moviereqgroup_movieshub')
             ],[
-            InlineKeyboardButton('ʜᴇʟᴘ', callback_data='help'),
-            InlineKeyboardButton('ᴀʙᴏᴜᴛ', callback_data='about')
+            InlineKeyboardButton('🍀 ʜᴇʟᴘ', callback_data='help'),
+            InlineKeyboardButton('🥀 ᴀʙᴏᴜᴛ', callback_data='about')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
