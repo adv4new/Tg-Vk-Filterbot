d538e59a85be97ee8addb7bcdfab4356cb4a4ed4
adv4new <107744568+adv4new@users.noreply.github.com>
2022-07-11 23:14:17 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -244,6 +244,7 @@ async def start(client, message):
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
+        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('ᴍᴏʀᴇ ᴍᴏᴠɪᴇ', url='https://t.me/Mh_linkZzz') ] ] ),
         protect_content=True if pre == 'filep' else False,
         )
                     
